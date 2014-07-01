5bc0673e460196821fefb3f3abffefeaa3f18938
Mike McNeil <mike@balderdash.co>
2014-07-01 12:10:25 -0500

    Added redis socket config+dummy falcon api to play w/

---
+++ api/controllers/FalconController.js
@@ -0,0 +1,11 @@
+/**
+ * FalconController
+ *
+ * @description :: Server-side logic for managing falcons
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
